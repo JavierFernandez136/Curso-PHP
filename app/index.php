0de3ee38602bc5169a795e11e89b6aa911891b8a
JavierFernandez136 <javier_fernandez_96@outlook.com>
2025-05-05 21:38:40 -0300

    paginacion

--- app/index.php
+++ app/index.php
@@ -2,6 +2,31 @@
 include 'includes/redirect.php';
  include 'includes/header.php';
 $users = mysqli_query($db,"SELECT * FROM users");
+$num_total_users = mysqli_num_rows($users);
+
+if($num_total_users > 0){
+    $rows_per_page = 3;
+    $page= false;
+
+    if(isset($_GET["page"])){
+        $page = $_GET["page"];
+    }
+
+    if(!$page){
+        $start = 0;
+        $page = 1;
+    }else{
+        $start = ($page-1) * $rows_per_page;
+    }
+
+    $total_pages = ceil($num_total_users / $rows_per_page);
+
+    $sql="SELECT * FROM users ORDER BY user_id DESC LIMIT {$start}, {$rows_per_page};";
+    $users = mysqli_query($db, $sql);
+
+}else{
+    echo "No hay usuarios";
+}
 ?>
 <table class="table">
     <tr>
@@ -28,6 +53,21 @@ $users = mysqli_query($db,"SELECT * FROM users");
 <?php }; ?>
     
 </table>
+
+<?php 
+    if($num_total_users >=1){ ?>
+    <ul class="pagination">
+        <li><a href="?page=<?=($page-1)?>"><<<</a></li>
+        <?php for($i = 1; $i <=$total_pages;$i++){ ?>
+            <?php if($page == $i){ ?>
+                <li><a href="#"><?=$i?></a></li>
+                <?php }else{?>
+                    <li><a href="?page=<?=$i?>"><?=$i?></a></li>
+                    <?php }?>
+        <?php }?>
+        <li><a href="?page=<?php $show_page= ($page+1); if($show_page<=$total_pages){ echo $show_page;}else{echo $total_pages;}?>">>>></a></li>
+    </ul>
+<?php }?>
 
 <?php 
  include 'includes/footer.php';
